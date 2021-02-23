1248d2d04af2e0b39851184ef8e4c77f8a43a23b
Sercan Sever <sercan.sever16@gmail.com>
2021-02-24 00:28:46 +0300

    24.02.21

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -6,7 +6,7 @@ namespace Business.Constants
 {
     public static class Messages
     {
-        public static string ProductAdded = "Ürün Eklnedi.";
+        public static string ProductAdded = "Ürün Eklendi.";
         public static string ProductNameInValid = "Ürün ismi geçersiz.";
         public static string ProductDeleted = "Ürün Silindi.";
         public static string ProductUpdated = "Ürün Güncellendi.";
