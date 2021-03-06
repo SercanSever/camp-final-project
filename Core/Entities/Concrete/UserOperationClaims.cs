e53de53bbde8212d8ab41ae1e942d2fdeea68c45
Sercan Sever <sercan.sever16@gmail.com>
2021-03-07 00:57:16 +0300

    07.03.21

--- Core/Entities/Concrete/UserOperationClaims.cs
+++ Core/Entities/Concrete/UserOperationClaims.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Core.Entities.Concrete
 {
-    public class UserOperationClaims
+    public class UserOperationClaims : IEntity
     {
         public int Id { get; set; }
         public int UserId { get; set; }
